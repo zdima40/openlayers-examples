7fc1a82cf570b359e0c8397bdc7b394baf526065
Dmitry Zhukov <DZhukov@k-tech.ru>
2022-09-30 13:21:52 +0300

    Add read geojson example

---
+++ examples/3-read-geojson/main.js
@@ -0,0 +1,39 @@
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+
+async function main() {
+  const response = await fetch('/polygons.json')
+  const json = await response.json();
+
+  const vectorSource = new VectorSource({
+    features: new GeoJSON({ featureProjection: 'EPSG:3857' }).readFeatures(json),
+  });
+
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+  
+  function createMap() {
+    return new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        }),
+        vectorLayer
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      }),
+    })
+  }
+  
+  const map = createMap();
+}
+
+main();
